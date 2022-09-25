991e46cacf477f6cb5b0ed417dc376078bddb433
33patburke <33patburke@gmail.com>
2022-09-25 01:03:09 -0400

    failed 3sum come on bro

--- neetCode150/02twoPointers/0015threeSum.py
+++ neetCode150/02twoPointers/0015threeSum.py
@@ -1,36 +1,38 @@
-# test case
-nums1 = [-1, 0, 1, 2, -1, -4]
-nums2 = [-3, 3, 4, -3, 1, 2]
-
 def threeSum(nums):
+
     res = []
     nums.sort()
-
-    for i, x in enumerate(nums):
-        # this checks if the x value on this iteration is a duplicate, i.e. we've seen it before so we
-        # don't want to waste our time checking again
-        if i > 0 and x == nums[i - 1]:
+    
+    for i in range(len(nums)):
+        ''' this checks if the num on this iteration is a duplicate, i.e. we've 
+            seen it before so we don't want to waste our time checking again '''
+        if i > 0 and nums[i] == nums[i-1]:
             continue
-
+        
         l = i + 1
         r = len(nums) - 1
         while l < r:
-            curr_sum = x + nums[l] + nums[r]
+            curr_sum = (nums[i] + nums[l] + nums[r])
             # sum is too small, increment left pointer
             if curr_sum < 0:
                 l += 1
-            # sum is too big, decrement right pointer
+            # sum is too small, increment left pointer
             elif curr_sum > 0:
                 r -= 1
-            # we found a valid threeSum
             else:
-                # add all values to result array
-                res.append([x, nums[l], nums[r]])
-                # also need to increment left pointer to keep scan going
+                res.append([nums[i], nums[l], nums[r]])
+                # keep scan going, could be another valid 3sum using this num
                 l += 1
                 # also need to make sure once again that we're not using a duplicate 
-                while nums[l] == nums[l - 1] and l < r:
-                    l += 1
+                #while nums[l] == nums[l - 1] and l < r:
+                #       l += 1
     return res
 
-threeSum(nums1)
+# test cases
+testcase1 = [-1,0,1,2,-1,-4] # [[-1,-1,2],[-1,0,1]]
+print(threeSum(testcase1))
+
+testcase2 = [0,0,0,0] # [0,0,0,0]
+print(threeSum(testcase2))
+
+# TC: O(nlogn) + O(n^2) = O(n^2)
